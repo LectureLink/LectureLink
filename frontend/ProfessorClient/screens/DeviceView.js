e6d2b2eb752bd96d39d4de7ba6f3afcac2226533
Evan Cook <cook.evank@gmail.com>
2023-04-12 14:22:32 -0400

    complete navigator setup

---
+++ frontend/ProfessorClient/screens/DeviceView.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+function DeviceView() {
+  return (
+    <View>
+      <Text>Device View</Text>
+    </View>
+  );
+}
+
+export default DeviceView;
